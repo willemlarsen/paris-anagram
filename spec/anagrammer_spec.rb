6f8cf9d7352c630834f49be4de3296b568e528b9
Willem Larsen <willemlarsen@gmail.com>
2015-02-16 07:39:50 -0800

    Let's warm up

---
+++ spec/anagrammer_spec.rb
@@ -0,0 +1,9 @@
+describe "a unit test" do
+  xit "should produce a red bar when test is (obviously) false" do
+    expect(1).to eq(2)
+  end
+
+  it "should produce a green bar when test is (obviously) true" do
+    expect(1).to eq(1)
+  end
+end
