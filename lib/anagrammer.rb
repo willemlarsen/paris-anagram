8637f64753d2e5e53ae1a904422f5ad436fcd86b
Willem Larsen <willemlarsen@gmail.com>
2015-03-02 08:02:24 -0800

    Remove duplication

--- lib/anagrammer.rb
+++ lib/anagrammer.rb
@@ -4,7 +4,7 @@ end
 
 def anagrams(word)
   return [word] if word.length == 1
-  first = word[0] + remove_nth_char(0, word)
-  second = word[1] + remove_nth_char(1, word)
-  [first, second]
+  (0...word.length).map do |i|
+    word[i] + remove_nth_char(i, word)
+  end
 end
