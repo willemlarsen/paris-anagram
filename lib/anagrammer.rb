e3f061f8ccdf2f5a6121c6115a440472b3353444
Willem Larsen <willemlarsen@gmail.com>
2015-03-11 09:33:42 -0700

    Push lower-level methods down in the file

--- lib/anagrammer.rb
+++ lib/anagrammer.rb
@@ -1,7 +1,3 @@
-def remove_nth_char(position, string)
-  string.slice(0, position) + string.slice(position + 1, string.length - 1 - position)
-end
-
 def anagrams(word)
   return [word] if word.length == 1
   (0...word.length).map do |i|
@@ -9,6 +5,10 @@ def anagrams(word)
     sub_word = remove_nth_char(i, word)
     distribute_char_over_sub_anagrams(char, anagrams(sub_word))
   end.flatten.uniq
+end
+
+def remove_nth_char(position, string)
+  string.slice(0, position) + string.slice(position + 1, string.length - 1 - position)
 end
 
 def distribute_char_over_sub_anagrams(char, sub_anagrams)
